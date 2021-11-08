47f3f64844b0a9b9103671a21ee20a33acc88f7c
unknown <mpanza.vusi5@gmail.com>
2021-11-08 16:02:41 +0200

    names added

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const names =["Vusumuzi","Andisiwe","Happy","lucky","Mduduzi","Seline","Khumbulani","Wand"]
+const names =["Vusumuzi","Andisiwe","Happy","lucky","Mduduzi","Seline","Khumbulani","Wandile","Kagisho","Nkosiphendule","Precious","Sithembiso","Ntombela","Mmapaseka","Sophie"]
 
 const display =
